577dcf0ac3c2f4a0a7a22ea8a4a2271e9a581f95
Hai Dang <haidang@hais-mbp.aavn.local>
2020-08-27 14:47:52 +0700

    Get total Score

--- lib/main.dart
+++ lib/main.dart
@@ -21,20 +21,38 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Dog', 'Cat', 'Lion', 'Chicken'],
+      'answers': [
+        {'text': 'Dog', 'score': 3},
+        {'text': 'Cat', 'score': 5},
+        {'text': 'Lion', 'score': 10},
+        {'text': 'Chicken', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite teacher?',
-      'answers': ['Hai', 'Ha', 'Bo', 'Noone'],
+      'answers': [
+        {'text': 'Hai', 'score': 1},
+        {'text': 'Ha', 'score': 1},
+        {'text': 'Bo', 'score': 1},
+        {'text': 'Someone', 'score': 1},
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -59,7 +77,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
